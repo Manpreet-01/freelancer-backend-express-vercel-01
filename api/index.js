f52228dec0ce90589f65fae8c8bfd9c49288ce46
Manpreet Singh <manpreetmss400@gmail.com>
2024-06-10 19:51:09 +0530

    vercel-express hello world deployed on vercel

--- api/index.js
+++ api/index.js
@@ -1,24 +1,14 @@
-const express = require('express');
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from 'express';
 const app = express();
 
-const path = require('path');
-
-
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, '..', 'components', 'home.htm'));
+	res.json({ res: process.env.PORT, msg: 'OK' });
 });
-
-app.get('/about', function (req, res) {
-	res.sendFile(path.join(__dirname, '..', 'components', 'about.htm'));
-});
-
-app.get('/test', function (req, res) {
-	res.json({ message: "hello world from express-vercel project." });
-});
-
-console.log("hit")
 
 // const port = 8000;
 // app.listen(port, () => console.log('Server ready on port ', port));
 
-module.exports = app;
+export default app;
