767d74f39446a54109ac9671c3341f3c893556e2
Manpreet Singh <manpreetmss400@gmail.com>
2024-06-17 13:10:40 +0530

    cors handling

--- api/index.js
+++ api/index.js
@@ -5,16 +5,28 @@ import express from 'express';
 import { connectDB } from "./db.js";
 import { userRouter } from "./routes/user.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
+import cors from "cors";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 
 
-app.use(express.json({limit: "1kb"}))
-app.use(express.urlencoded({extended: true, limit: "1kb"}))
+app.use(express.json({ limit: "1kb" }));
+app.use(express.urlencoded({ extended: true, limit: "1kb" }));
+
+app.use(
+	cors({
+		origin:
+			process.env.CORS_ORIGIN === "*"
+				? "*" // This might give CORS error for some origins due to credentials set to true
+				: process.env.CORS_ORIGIN?.split(","), // For multiple cors origin for production. Refer https://github.com/hiteshchoudhary/apihub/blob/a846abd7a0795054f48c7eb3e71f3af36478fa96/.env.sample#L12C1-L12C12
+		credentials: true,
+	})
+);
 
 
-app.get('/', function (req, res) {
+
+app.get('/', (req, res) => {
 	res.json({ res: PORT, msg: 'Server is OK' });
 });
 
